a2db736ac63cafc22636d3631e9a3d6cfe44cafb
Elektri <stefaniebissinger@StefanisMacBook.fritz.box>
2017-03-12 17:58:27 +0100

    Design of password reset page
    - added password reset page
    - added Slider to home/index page als an example (just for better
    looking on Thursday presentaion)

--- src/Inventory.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ src/Inventory.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -10,17 +10,17 @@ namespace Athene.Inventory.Web.Models.ManageViewModels
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Aktuelles Passwort")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Neues Passwort")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Neues Passwort bestätigen")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
